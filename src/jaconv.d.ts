77f5c591bc01f4188630694ce8961278aca9c229
ARISTOS <storm1legendaryhero@gmail.com>
2024-10-30 18:54:23 +0900

    Updates

--- src/jaconv.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'jaconv' {
-  export function toHiragana(str: string): string;
-  export function toKatakana(str: string): string;
-  export function toHan(str: string): string;
-  export function toZen(str: string): string;
-}
