f3d16ab8c4a0a85b2d06a64c89f15365017e968b
OCEANS7A-DEV <oceans7a@gmail.com>
2025-01-31 15:31:38 +0900

    コードで検索

--- src/backend/WebStorage.ts
+++ src/backend/WebStorage.ts
@@ -81,26 +81,31 @@ export const localExclusion = (store: string, pageName: string) => {
 
 
 export const searchStr = async (searchword: string) => {
+  //console.log(searchword)
   const swKZ = jaconv.toKatakana(searchword);
   const swHZ = jaconv.toHiragana(swKZ);
   const swKH = jaconv.toHan(swKZ);
-  const data = JSON.parse(sessionStorage.getItem('data') ?? '');
+  const data = JSON.parse(localStorage.getItem('data') ?? '');
   if (!data || data.length === 0) {
     console.log('データが存在しません。');
     return [];
   }
+
   const result = data.filter((item: any[]) => {
     const productName = item[2];
+    const productCode = String(item[1]);
     if (typeof productName !== 'string') {
-      console.log('商品名が文字列ではありません:', productName);
       return false;
     }
     return (
       productName.indexOf(swKZ) !== -1 ||
       productName.indexOf(swKH) !== -1 ||
-      productName.indexOf(swHZ) !== -1
+      productName.indexOf(swHZ) !== -1 ||
+      productCode.indexOf(searchword) !== -1
     );
   });
+
+  //console.log(result)
   const FiltereResult = result.filter(row => row[2] !== "");
   const Last = FiltereResult.filter(row => Number.isInteger(row[1]));
   return Last;
